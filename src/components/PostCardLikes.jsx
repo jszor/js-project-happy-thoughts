8cd827bb836221a717442aaac0e75ecf58be7d39
Juan Salvador Zorrilla <jsalvadorzorrilla@gmail.com>
2025-05-05 23:11:12 +0200

    implemented useState to change color of like button when clicked

--- src/components/PostCardLikes.jsx
+++ src/components/PostCardLikes.jsx
@@ -1,13 +1,15 @@
 import styled from 'styled-components'
+import { useState } from 'react'
 
 const HeartStyled = styled.span`
-  background-color: #EAEAEA;
+  background-color: ${({ liked }) => liked ? '#FFADAD' : '#EAEAEA'};
   border: none;
   border-radius: 50%;
   padding: 0.625rem;
   text-align: center;
   text-decoration: none;
   font-size: 1rem;
+  cursor: pointer;
 `
 
 const LikesStyled = styled.span`
@@ -17,9 +19,15 @@ const LikesStyled = styled.span`
 `
 
 const PostCardLikes = ({ likes }) => {
+
+  const [liked, setLiked] = useState(false)
+
+  const handleLike = () => {
+    setLiked(!liked)
+  }
   return (
     <div>
-      <HeartStyled>❤️</HeartStyled>
+      <HeartStyled liked={liked} onClick={handleLike}>❤️</HeartStyled>
       <LikesStyled>x {likes}</LikesStyled>
     </div>
   )
