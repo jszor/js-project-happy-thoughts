3510055ab435f9bff312ca03963b8a3c8b80342a
Juan Salvador Zorrilla <jsalvadorzorrilla@gmail.com>
2025-05-05 23:01:45 +0200

    added post card container for post cards

--- src/App.jsx
+++ src/App.jsx
@@ -7,13 +7,19 @@ const AppWrapper = styled.div`
   font-family: "Inter", sans-serif;
 `
 
+const PostWrapper = styled.div`
+  margin-top: 1rem;
+`
+
 export const App = () => {
   return (
     <AppWrapper>
       <CreatePostCard />
-      <PostCard message="I'm happy because we just moved into a new apartment!" likes="0" time="30 seconds ago" />
-      <PostCard message="It's my birthday!" likes="10" time="10 minutes ago" />
-      <PostCard message="I'm happy because the sun is out :)" likes="23" time="15 minutes ago" />
+      <PostWrapper>
+        <PostCard message="I'm happy because we just moved into a new apartment!" likes="0" time="30 seconds ago" />
+        <PostCard message="It's my birthday!" likes="10" time="10 minutes ago" />
+        <PostCard message="I'm happy because the sun is out :)" likes="23" time="15 minutes ago" />
+      </PostWrapper>
     </AppWrapper>
   )
 }
